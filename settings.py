2910c6c30ceeacce8e57aa9369aad7e90b5c5301
leonamcassemir0 <114764927+leonamcassemir0@users.noreply.github.com>
2024-04-09 19:57:44 -0300

    cpmecei o primeiro projeto

---
+++ settings.py
@@ -0,0 +1,17 @@
+"""
+Sempre que for criar um projeto, crie um arquivo para configurações padrão,
+para que você não precise modificar sempre cada arquivo do projeto.
+"""
+
+
+class Settings():
+    """
+    Uma classe para armazenar todas as configurações da Invasão Alienpigena.
+    """
+
+    def __init__(self):
+        # Inicializa as configurações do jogo
+        # Configurações da tela:
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
