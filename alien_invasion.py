f25329780e185c64c3a2afd46f3585f114f40de4
leonamcassemir0 <114764927+leonamcassemir0@users.noreply.github.com>
2024-04-09 20:12:47 -0300

    x

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 
 def run_game():
@@ -10,6 +11,8 @@ def run_game():
     screen = pygame.display.set_mode((
         ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+    # Cria uma espaçonave
+    ship = Ship(screen)
     # Inicia o laço principal do jogo
     while True:
         # Observa eventos de teclado e de mouse
@@ -18,6 +21,7 @@ def run_game():
                 sys.exit()
             # Redesenha a tela a cada passagem pelo laço
             screen.fill(ai_settings.bg_color)
+            ship.blitme()
 
         # Deixa a tela mais recente visível
         pygame.display.flip()
